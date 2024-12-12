983c4c051d0129b99c24ce8a1b7e425f83501e0f
facutota <facutota@gmail.com>
2024-12-12 10:50:41 -0300

    habciendo funcionar

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/src', 
-  server: {
-    strictPort: true,
-    // Puedes agregar headers aquí si necesitas asegurar el tipo MIME
-    headers: {
-      'Content-Type': 'application/javascript',
-    },
-  },
-});
+})
